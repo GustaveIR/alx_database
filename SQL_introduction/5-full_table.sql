81c554533065d733114d68d54e9fe7cc13dfbd4f
GustaveIR <iraguhagustave@gmail.com>
2023-08-15 19:26:39 +0200

    SQL_introduction

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -6,17 +6,17 @@ SET @create_statement = (
         GROUP_CONCAT(
             '  `', column_name, '` ', column_type,
             IF(is_nullable = 'NO', 'NOT NULL', 'DEFAULT NULL'),
-            IF(column_key = 'PRI', 'AUTO_INCREMENT', ''),
+            IF(column_key = 'PRI', ' AUTO_INCREMENT', ''),
             IF(column_default IS NOT NULL AND column_key <> 'PRI', CONCAT(' DEFAULT ', IF(column_default = '', 'NULL', column_default)), ''),
             IF(column_key = 'PRI', '', IF(is_nullable = 'NO', ', PRIMARY KEY (`id`)', '')),
-            ',\n'
+            '\n'
         ),
-        ') ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;'
+        '); ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;'
     )
     FROM information_schema.columns
-    WHERE table_schema = 'hbtn_test_db_5'
+    WHERE table_schema = 'hbtn_0c_0'
       AND table_name = 'first_table'
 );
 
 -- Print the formatted CREATE TABLE statement
-SELECT REPLACE(REPLACE(@create_statement, ",`", "`"), ",\n)", "\n);") AS formatted_create_statement;
+SELECT REPLACE(REPLACE(@create_statement, ",`", "`"), "\n);", ");") AS formatted_create_statement;
