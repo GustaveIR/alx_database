4c84ead34b908218e9b71ce827c5cd3dc31b0c45
GustaveIR <iraguhagustave@gmail.com>
2023-08-15 18:47:53 +0200

    SQL_introduction

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -2,19 +2,19 @@
 SET @create_statement = (
     SELECT CONCAT(
         table_name, '\n',
-        'CREATE TABLE `', table_name, '` (\n',
+        'CREATETABLE`', table_name, '`(\n',
         GROUP_CONCAT(
-            '  `', column_name, '` ', column_type,
-            IF(is_nullable = 'NO', 'NOT NULL', 'DEFAULT NULL'),
+            '  `', column_name, '`', column_type,
+            IF(is_nullable = 'NO', 'NOTNULL', 'DEFAULTNULL'),
+            IF(column_default IS NOT NULL AND column_key <> 'PRI', CONCAT('DEFAULT', IF(column_default = '', 'NULL', column_default)), ''),
             IF(column_key = 'PRI', 'AUTO_INCREMENT', ''),
-            IF(column_default IS NOT NULL AND column_key <> 'PRI', CONCAT('DEFAULT ', IF(column_default = '', 'NULL', column_default)), ''),
-            IF(column_key = 'PRI', '', IF(is_nullable = 'NO', 'PRIMARY KEY', '')),
-            SEPARATOR ',\n'
+            ',\n'
         ),
-        '\n) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;'
+        'PRIMARYKEY(`id`)\n',
+        ')ENGINE=InnoDBDEFAULTCHARSET=utf8mb4COLLATE=utf8mb4_0900_ai_ci;'
     )
     FROM information_schema.columns
-    WHERE table_schema = 'hbtn_0c_0'
+    WHERE table_schema = 'hbtn_test_db_5'
       AND table_name = 'first_table'
 );
 
