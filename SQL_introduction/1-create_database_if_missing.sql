20be5f6dd49c998111cb6c1b32c748d50a42b3cb
GustaveIR <iraguhagustave@gmail.com>
2023-08-15 16:02:05 +0200

    SQL_introduction

--- SQL_introduction/1-create_database_if_missing.sql
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -1,1 +1,10 @@
-CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
+-- Check if the database already exists
+SET @db_exists = (SELECT COUNT(*) FROM information_schema.schemata WHERE schema_name = 'hbtn_0c_0');
+
+-- Create the database if it doesn't exist
+IF @db_exists = 0 THEN
+    CREATE DATABASE hbtn_0c_0;
+    SELECT 'Database created: hbtn_0c_0' AS Message;
+ELSE
+    SELECT 'Database already exists: hbtn_0c_0' AS Message;
+END IF;
