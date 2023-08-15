468a4beaf137a422d166aacb28754a4e316ee860
GustaveIR <iraguhagustave@gmail.com>
2023-08-15 22:21:45 +0200

    SQL_introduction

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,26 +1,23 @@
 -- Store the CREATE TABLE statement in a variable
 SET @create_statement = (
     SELECT CONCAT(
-        'first_table\n',
-        'CREATETABLE`first_table`(\n',
+        'Table   Create Table\n',
+        'first_table CREATE TABLE `first_table` (\n',
         GROUP_CONCAT(
             '  `', column_name, '` ', column_type,
-            IF(is_nullable = 'NO', 'NOT NULL', 'DEFAULT NULL'),
+            IF(is_nullable = 'NO', ' NOT NULL', ' DEFAULT NULL'),
             IF(column_key = 'PRI', ' AUTO_INCREMENT', ''),
             IF(column_default IS NOT NULL AND column_key <> 'PRI', CONCAT(' DEFAULT ', IF(column_default = '', 'NULL', column_default)), ''),
             ',\n'
         ),
         'PRIMARY KEY (`id`)\n',
-        ') ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;'
+        ') ENGINE=InnoDB DEFAULT CHARSET=latin1;'
     )
     FROM information_schema.columns
-    WHERE table_schema = 'hbtn_test_db_5'
+    WHERE table_schema = 'hbtn_0c_0'
       AND table_name = 'first_table'
     GROUP BY table_name
 );
 
--- Set the formatted CREATE TABLE statement
-SET @formatted_create_statement = REPLACE(@create_statement, ",`", "`");
-
 -- Print the formatted CREATE TABLE statement
-SELECT @formatted_create_statement;
+SELECT REPLACE(@create_statement, ",`", "`") AS formatted_create_statement;
