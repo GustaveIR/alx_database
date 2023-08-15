ce4d854441461687839a8e564162d36f5cc3a30a
GustaveIR <iraguhagustave@gmail.com>
2023-08-15 19:22:56 +0200

    SQL_introduction

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,17 +1,17 @@
 -- Store the CREATE TABLE statement in a variable
 SET @create_statement = (
     SELECT CONCAT(
-        table_name, '\n',
-        'CREATETABLE`', table_name, '`(\n',
+        'first_table\n',
+        'CREATETABLE `first_table` (\n',
         GROUP_CONCAT(
             '  `', column_name, '` ', column_type,
-            IF(is_nullable = 'NO', 'NOTNULL', 'DEFAULTNULL'),
-            IF(column_default IS NOT NULL AND column_key <> 'PRI', CONCAT('DEFAULT ', IF(column_default = '', 'NULL', column_default)), ''),
+            IF(is_nullable = 'NO', 'NOT NULL', 'DEFAULT NULL'),
             IF(column_key = 'PRI', 'AUTO_INCREMENT', ''),
+            IF(column_default IS NOT NULL AND column_key <> 'PRI', CONCAT(' DEFAULT ', IF(column_default = '', 'NULL', column_default)), ''),
+            IF(column_key = 'PRI', '', IF(is_nullable = 'NO', ', PRIMARY KEY (`id`)', '')),
             ',\n'
         ),
-        'PRIMARYKEY(`id`)\n',
-        ')ENGINE=InnoDBDEFAULTCHARSET=utf8mb4COLLATE=utf8mb4_0900_ai_ci;'
+        ') ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;'
     )
     FROM information_schema.columns
     WHERE table_schema = 'hbtn_test_db_5'
