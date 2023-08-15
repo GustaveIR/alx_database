b74cc151f6cef99dc14be05f0ef05850dd6752f0
GustaveIR <iraguhagustave@gmail.com>
2023-08-15 16:03:35 +0200

    SQL_introduction

--- SQL_introduction/1-create_database_if_missing.sql
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -1,10 +1,15 @@
--- Check if the database already exists
-SET @db_exists = (SELECT COUNT(*) FROM information_schema.schemata WHERE schema_name = 'hbtn_0c_0');
+-- Attempt to create the database (will fail if it already exists)
+CREATE DATABASE hbtn_0c_0;
 
--- Create the database if it doesn't exist
-IF @db_exists = 0 THEN
-    CREATE DATABASE hbtn_0c_0;
-    SELECT 'Database created: hbtn_0c_0' AS Message;
-ELSE
+-- Check if the database was created successfully
+-- The error code for "database already exists" is 1007
+-- We ignore this error using the IF statement
+SET @error_code = (SELECT IF(COUNT(*) = 0, 0, 1) FROM mysql.errors WHERE error_code = 1007);
+
+-- If @error_code is 1, the database already exists
+-- If it's 0, the database was successfully created
+IF @error_code = 1 THEN
     SELECT 'Database already exists: hbtn_0c_0' AS Message;
+ELSE
+    SELECT 'Database created: hbtn_0c_0' AS Message;
 END IF;
