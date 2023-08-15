8d20b9f91125cf4e6ab87526bbff53298f19b859
GustaveIR <iraguhagustave@gmail.com>
2023-08-15 17:05:02 +0200

    SQL_introduction

--- SQL_introduction/1-create_database_if_missing.sql
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -1,9 +1,12 @@
+-- Use a default database to avoid "No database selected" error
+USE mysql;
+
 -- Create a stored procedure that attempts to create the database
 DELIMITER //
 CREATE PROCEDURE CreateDatabaseIfNotExists()
 BEGIN
     DECLARE CONTINUE HANDLER FOR 1007 BEGIN END;
-    CREATE DATABASE hbtn_0c_0;
+    CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
 END;
 //
 DELIMITER ;
---
+++ SQL_introduction/main_0_1.sql
@@ -0,0 +1,4 @@
+-- Use the hbtn_0c_0 database
+USE hbtn_0c_0;
+
+-- Your query here (if needed)
