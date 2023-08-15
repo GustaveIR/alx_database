d4d4e9e67f098d2efe60c5cc2d7353fabe7f032f
GustaveIR <iraguhagustave@gmail.com>
2023-08-15 17:15:04 +0200

    SQL_introduction

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,5 +1,17 @@
--- Print the full description of the table first_table
-SELECT table_name, create_statement
-FROM information_schema.tables
-WHERE table_schema = 'hbtn_0c_0'
-  AND table_name = 'first_table';
+-- Store the CREATE TABLE statement in a temporary table
+CREATE TEMPORARY TABLE IF NOT EXISTS tmp_create_table AS
+    SELECT create_statement
+    FROM information_schema.tables
+    WHERE table_schema = 'hbtn_0c_0'
+      AND table_name = 'first_table';
+
+-- Retrieve column definitions
+SELECT 
+    REPLACE(
+        REPLACE(
+            REPLACE(
+                REPLACE(create_statement, '`', ''),
+                '\n', ''),
+            '  ', ' '),
+        ',', ',\n') AS create_statement
+FROM tmp_create_table;
