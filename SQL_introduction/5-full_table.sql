213416ab46e85f4f24fff743fa6f581a53d44800
GustaveIR <iraguhagustave@gmail.com>
2023-08-15 19:36:25 +0200

    SQL_introduction

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -9,9 +9,9 @@ SET @create_statement = (
             IF(column_key = 'PRI', ' AUTO_INCREMENT', ''),
             IF(column_default IS NOT NULL AND column_key <> 'PRI', CONCAT(' DEFAULT ', IF(column_default = '', 'NULL', column_default)), ''),
             IF(column_key = 'PRI', '', IF(is_nullable = 'NO', ', PRIMARY KEY (`id`)', '')),
-            '\n'
+            ','
         ),
-        '); ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;'
+        '\n);\nENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;'
     )
     FROM information_schema.columns
     WHERE table_schema = 'hbtn_0c_0'
@@ -19,4 +19,4 @@ SET @create_statement = (
 );
 
 -- Print the formatted CREATE TABLE statement
-SELECT REPLACE(REPLACE(@create_statement, ",`", "`"), "\n);", ");") AS formatted_create_statement;
+SELECT REPLACE(@create_statement, ",`", "`") AS formatted_create_statement;
