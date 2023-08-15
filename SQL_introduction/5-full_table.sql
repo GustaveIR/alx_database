73eab56d0b76868b1bc54f3119a91a1449e1ac53
GustaveIR <iraguhagustave@gmail.com>
2023-08-15 17:16:36 +0200

    SQL_introduction

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,17 +1,25 @@
--- Store the CREATE TABLE statement in a temporary table
-CREATE TEMPORARY TABLE IF NOT EXISTS tmp_create_table AS
+-- Store the CREATE TABLE statement in a variable
+SET @create_statement = (
     SELECT create_statement
     FROM information_schema.tables
     WHERE table_schema = 'hbtn_0c_0'
-      AND table_name = 'first_table';
+      AND table_name = 'first_table'
+);
 
--- Retrieve column definitions
-SELECT 
-    REPLACE(
-        REPLACE(
-            REPLACE(
-                REPLACE(create_statement, '`', ''),
-                '\n', ''),
-            '  ', ' '),
-        ',', ',\n') AS create_statement
-FROM tmp_create_table;
+-- Retrieve column definitions and format the output
+SELECT GROUP_CONCAT(column_line SEPARATOR '\n') AS formatted_create_statement
+FROM (
+    SELECT
+        CONCAT(
+            REPLACE(column_name, '`', ''),
+            ' ',
+            column_type,
+            IF(column_default IS NOT NULL, CONCAT(' DEFAULT ', column_default), ''),
+            IF(is_nullable = 'NO', ' NOT NULL', ''),
+            IF(column_key = 'PRI', ' PRIMARY KEY', ''),
+            IF(extra = 'auto_increment', ' AUTO_INCREMENT', '')
+        ) AS column_line
+    FROM information_schema.columns
+    WHERE table_schema = 'hbtn_0c_0'
+      AND table_name = 'first_table'
+) AS formatted_columns;
