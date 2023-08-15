49f61484df486c0acd8f4fd513aa35db6a420910
GustaveIR <iraguhagustave@gmail.com>
2023-08-15 17:18:26 +0200

    SQL_introduction

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -7,19 +7,8 @@ SET @create_statement = (
 );
 
 -- Retrieve column definitions and format the output
-SELECT GROUP_CONCAT(column_line SEPARATOR '\n') AS formatted_create_statement
-FROM (
-    SELECT
-        CONCAT(
-            REPLACE(column_name, '`', ''),
-            ' ',
-            column_type,
-            IF(column_default IS NOT NULL, CONCAT(' DEFAULT ', column_default), ''),
-            IF(is_nullable = 'NO', ' NOT NULL', ''),
-            IF(column_key = 'PRI', ' PRIMARY KEY', ''),
-            IF(extra = 'auto_increment', ' AUTO_INCREMENT', '')
-        ) AS column_line
-    FROM information_schema.columns
-    WHERE table_schema = 'hbtn_0c_0'
-      AND table_name = 'first_table'
-) AS formatted_columns;
+SELECT 
+    CONCAT(table_name, 'CREATE TABLE', create_statement) AS formatted_create_statement
+FROM information_schema.tables
+WHERE table_schema = 'hbtn_0c_0'
+  AND table_name = 'first_table';
