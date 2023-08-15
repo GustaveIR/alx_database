b05cc935b68a16386d18ac821831878390a77dca
GustaveIR <iraguhagustave@gmail.com>
2023-08-15 16:56:40 +0200

    SQL_introduction

--- SQL_introduction/1-create_database_if_missing.sql
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -1,15 +1,12 @@
 -- Attempt to create the database (will fail if it already exists)
 CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
 
--- Check if the database was created successfully
--- The error code for "database already exists" is 1007
--- We ignore this error using the IF statement
-SET @error_code = (SELECT IF(COUNT(*) = 0, 0, 1) FROM mysql.errors WHERE error_code = 1007);
+-- Create a temporary table to store the result of the attempt
+CREATE TEMPORARY TABLE IF NOT EXISTS create_db_result (message VARCHAR(100));
 
--- If @error_code is 1, the database already exists
--- If it's 0, the database was successfully created
-IF @error_code = 1 THEN
-    SELECT 'Database already exists: hbtn_0c_0' AS Message;
-ELSE
-    SELECT 'Database created: hbtn_0c_0' AS Message;
-END IF;
+-- Insert a message based on the attempt result
+INSERT INTO create_db_result (message) VALUES
+    (IFNULL(NULLIF(LAST_INSERT_ID(CONNECTION_ID()), 0), 'Database already exists: hbtn_0c_0'));
+
+-- Display the result
+SELECT message FROM create_db_result;
