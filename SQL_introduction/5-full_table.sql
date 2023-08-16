97537f7ecef44743d4e303cbe697153c6daa178a
GustaveIR <iraguhagustave@gmail.com>
2023-08-16 09:23:10 +0200

    SQL_introduction

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,15 +1,15 @@
 -- Store the CREATE TABLE statement in a variable
 SET @create_statement = (
     SELECT CONCAT(
-        'first_table CREATE TABLE `first_table` (\n',
+        'first_table CREATE TABLE `first_table` (',
         GROUP_CONCAT(
-            '  `', column_name, '` ', column_type,
+            '`', column_name, '` ', column_type,
             IF(is_nullable = 'NO', ' NOT NULL', ' DEFAULT NULL'),
             IF(column_key = 'PRI', ' AUTO_INCREMENT', ''),
             IF(column_default IS NOT NULL AND column_key <> 'PRI', CONCAT(' DEFAULT ', IF(column_default = '', 'NULL', column_default)), ''),
             ','
         ),
-        '\n  PRIMARY KEY (`id`)\n',
+        'PRIMARY KEY (`id`)',
         ') ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;'
     )
     FROM information_schema.columns
@@ -19,4 +19,4 @@ SET @create_statement = (
 );
 
 -- Print the formatted CREATE TABLE statement
-SELECT REPLACE(@create_statement, ',`', '`') AS formatted_create_statement;
+SELECT @create_statement AS formatted_create_statement;
