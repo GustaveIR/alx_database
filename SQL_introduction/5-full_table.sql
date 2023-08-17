6431434e6a163b8c385c44c0fc5a57377b72a1bf
GustaveIR <iraguhagustave@gmail.com>
2023-08-17 10:26:02 +0200

    SQL_introduction

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,6 +1,11 @@
+-- Set the database and table names
+SET @db_name = 'hbtn_test_db_5';
+SET @table_name = 'first_table';
+
+-- Store the CREATE TABLE statement in a variable
 SET @create_statement = (
     SELECT CONCAT(
-        'first_table CREATE TABLE `first_table` (',
+        @table_name, ' CREATE TABLE `', @table_name, '` (',
         GROUP_CONCAT(
             '`', column_name, '` ', column_type,
             IF(is_nullable = 'NO', ' NOT NULL', ' DEFAULT NULL'),
@@ -12,9 +17,10 @@ SET @create_statement = (
         ') ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;'
     )
     FROM information_schema.columns
-    WHERE table_schema = 'hbtn_0c_0'
-      AND table_name = 'first_table'
+    WHERE table_schema = @db_name
+      AND table_name = @table_name
     GROUP BY table_name
 );
 
+-- Print the formatted CREATE TABLE statement
 SELECT @create_statement;
