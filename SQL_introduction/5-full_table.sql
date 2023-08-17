f1d961a3c8b564160d29821b442e1c5508f95884
GustaveIR <iraguhagustave@gmail.com>
2023-08-17 22:22:39 +0200

    SQL_introduction

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -5,9 +5,9 @@ SET @table_name = 'first_table';
 -- Get the column information
 SET @column_info = (
     SELECT GROUP_CONCAT(
-        '`', column_name, '` ', column_type,
+        '`', column_name, '`', ' ', column_type,
         IF(is_nullable = 'NO', ' NOT NULL', ' DEFAULT NULL'),
-        IF(column_key = 'PRI', ' PRIMARY KEY', ''),
+        IF(column_key = 'PRI', ' AUTO_INCREMENT', ''),
         IF(column_default IS NOT NULL AND column_key <> 'PRI', CONCAT(' DEFAULT ', IF(column_default = '', 'NULL', column_default)), '')
     )
     FROM information_schema.columns
@@ -19,6 +19,7 @@ SET @column_info = (
 SET @create_statement = CONCAT(
     @table_name, ' CREATE TABLE `', @table_name, '` (',
     @column_info,
+    '\nPRIMARY KEY (`id`)',
     '\n) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;'
 );
 
