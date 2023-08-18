b91249ca4c0e64d7de999da1d24abd0c006244fa
GustaveIR <iraguhagustave@gmail.com>
2023-08-18 23:39:28 +0200

    SQL_introduction

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -9,27 +9,3 @@ CREATE TABLE IF NOT EXISTS first_table (
     created_at DATE,
     PRIMARY KEY (id)
 );
-
--- Print the full description of the table
-SELECT
-    CONCAT('first_tableCREATETABLE`first_table`(', 
-           GROUP_CONCAT(
-               '`', COLUMN_NAME, '`', COLUMN_TYPE, 
-               IF(IS_NULLABLE = 'NO', 'NOTNULL', 'DEFAULTNULL'), 
-               IF(EXTRA = 'auto_increment', 'AUTO_INCREMENT', ''), ','
-           ),
-           'PRIMARYKEY(`id`)',
-           ')ENGINE=InnoDBDEFAULTCHARSET=utf8mb4COLLATE=utf8mb4_0900_ai_ci'
-    )
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_NAME = 'first_table';
-
--- Print the storage engine and character set of the table
-SELECT
-    CONCAT(
-        'first_table',
-        'ENGINE=', ENGINE,
-        ' DEFAULT CHARSET=', TABLE_COLLATION
-    )
-FROM INFORMATION_SCHEMA.TABLES
-WHERE TABLE_NAME = 'first_table';
