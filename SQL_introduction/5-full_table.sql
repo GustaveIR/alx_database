ca788d2f94fc2e7a7cb1a332dcfc75398f37d392
GustaveIR <iraguhagustave@gmail.com>
2023-08-18 12:43:22 +0200

    SQL_introduction

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -6,11 +6,8 @@ CREATE DATABASE IF NOT EXISTS hbtn_test_db_5;
 -- Use the database `hbtn_test_db_5`
 USE hbtn_test_db_5;
 
--- If the table `first_table` does not exist, create it
-IF NOT EXISTS (
-  SELECT 1 FROM information_schema.tables WHERE table_name = 'first_table'
-)
-CREATE TABLE first_table (
+-- Create the table `first_table` if it does not exist
+CREATE TABLE IF NOT EXISTS first_table (
   id INT NOT NULL AUTO_INCREMENT,
   name VARCHAR(128) NOT NULL,
   c CHAR(1) NOT NULL,
