cb9229a31a654b75165c836fcd9a2a273339b735
GustaveIR <iraguhagustave@gmail.com>
2023-08-19 00:33:50 +0200

    SQL_introduction

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -2,12 +2,10 @@
 DROP DATABASE IF EXISTS hbtn_test_db_5;
 CREATE DATABASE IF NOT EXISTS hbtn_test_db_5;
 USE hbtn_test_db_5;
-
--- Create first_table
 CREATE TABLE IF NOT EXISTS first_table (
     id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(128) DEFAULT NULL,
-    c CHAR(1) DEFAULT NULL,
-    created_at DATE DEFAULT NULL,
+    name VARCHAR(128),
+    c CHAR(1),
+    created_at DATE,
     PRIMARY KEY (id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+);
