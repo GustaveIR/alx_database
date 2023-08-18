6e309ad62555bdb2bd21faafe84d2abb7a0af8c2
GustaveIR <iraguhagustave@gmail.com>
2023-08-18 12:22:48 +0200

    SQL_introduction

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,27 +1,14 @@
--- Set the database and table names
-SET @db_name = 'hbtn_0c_0';
-SET @table_name = 'first_table';
-
--- Get the column information
-SET @column_info = (
-    SELECT GROUP_CONCAT(
-        '`', column_name, '`', column_type,
-        IF(is_nullable = 'NO', ' NOT NULL', ' DEFAULT NULL'),
-        IF(column_default IS NOT NULL AND column_key <> 'PRI', CONCAT(' DEFAULT ', IF(column_default = '', 'NULL', column_default)), ''),
-        IF(column_key = 'PRI', ' AUTO_INCREMENT', '')
-    )
-    FROM information_schema.columns
-    WHERE table_schema = @db_name
-      AND table_name = @table_name
-);
-
--- Construct the CREATE TABLE statement
-SET @create_statement = CONCAT(
-    @table_name, ' CREATE TABLE `', @table_name, '` (',
-    @column_info,
-    ', PRIMARY KEY (`id`)',
-    ') ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;'
-);
-
--- Print the formatted CREATE TABLE statement
-SELECT @create_statement;
+CREATE TABLE `first_table` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(128) DEFAULT NULL,
+  `c` char(1) DEFAULT NULL,
+  `created_at` date DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `first_table` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(128) DEFAULT NULL,
+  `c` char(1) DEFAULT NULL,
+  `created_at` date DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
