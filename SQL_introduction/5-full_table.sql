dfd29c547912b6729517c1ada9def16a9f05f321
GustaveIR <iraguhagustave@gmail.com>
2023-08-18 16:23:54 +0200

    SQL_introduction

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -2,9 +2,4 @@
 
 CREATE DATABASE hbtn_test_db_5;
 USE hbtn_test_db_5;
-CREATE TABLE t1 (
-         i1 INT CHECK (i1 <> 0),      -- column constraint
-         i2 INT,
-         CHECK (i2 > i1),             -- table constraint
-         CHECK (i2 <> 0) NOT ENFORCED -- table constraint, not enforced
-       );
+DROP TABLE IF EXISTS first_table;
