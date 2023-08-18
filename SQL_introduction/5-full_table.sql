8db752f311e19ad03221c15a962f7bf777d4e11b
GustaveIR <iraguhagustave@gmail.com>
2023-08-18 23:35:55 +0200

    SQL_introduction

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -11,21 +11,25 @@ CREATE TABLE IF NOT EXISTS first_table (
 );
 
 -- Print the full description of the table
-SELECT CONCAT(
-    'first_tableCREATE TABLE `first_table` (',
-    '`id`intNOT NULL AUTO_INCREMENT,',
-    '`name`varchar(128)DEFAULT NULL,',
-    '`c`char(1)DEFAULT NULL,',
-    '`created_at`dateDEFAULT NULL,',
-    'PRIMARY KEY (`id`)',
-    ') ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci'
-);
+SELECT
+    CONCAT('first_tableCREATETABLE`first_table`(', 
+           GROUP_CONCAT(
+               '`', COLUMN_NAME, '`', COLUMN_TYPE, 
+               IF(IS_NULLABLE = 'NO', 'NOTNULL', 'DEFAULTNULL'), 
+               IF(EXTRA = 'auto_increment', 'AUTO_INCREMENT', ''), ','
+           ),
+           'PRIMARYKEY(`id`)',
+           ')ENGINE=InnoDBDEFAULTCHARSET=utf8mb4COLLATE=utf8mb4_0900_ai_ci'
+    )
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'first_table';
 
 -- Print the storage engine and character set of the table
-SELECT CONCAT(
-    'first_table',
-    'ENGINE=', ENGINE,
-    ' DEFAULT CHARSET=', TABLE_COLLATION
-)
+SELECT
+    CONCAT(
+        'first_table',
+        'ENGINE=', ENGINE,
+        ' DEFAULT CHARSET=', TABLE_COLLATION
+    )
 FROM INFORMATION_SCHEMA.TABLES
 WHERE TABLE_NAME = 'first_table';
