62afec7900ee4fee0d675830791ea3fea4dafa89
GustaveIR <iraguhagustave@gmail.com>
2023-08-22 13:47:33 +0200

    SQL_introduction

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -5,15 +5,17 @@
 
 -- Use SHOW COLUMNS statement to get the table structure
 SELECT
-    CONCAT('first_tableCREATE TABLE`first_table`(',
-           GROUP_CONCAT(
-               '`', COLUMN_NAME, '`', COLUMN_TYPE,
-               CASE WHEN IS_NULLABLE = 'NO' THEN 'NOTNULL' ELSE 'DEFAULTNULL' END,
-               CASE WHEN COLUMN_DEFAULT IS NOT NULL THEN CONCAT('DEFAULT', COLUMN_DEFAULT) ELSE '' END,
-               ','
-           ),
-           'PRIMARYKEY(`id`)'
-           ')ENGINE=InnoDBDEFAULTCHARSET=utf8mb4COLLATE=utf8mb4_0900_ai_ci'
+    CONCAT(
+        'first_tableCREATETABLE`first_table`(',
+        GROUP_CONCAT(
+            '`', COLUMN_NAME, '`', COLUMN_TYPE,
+            CASE WHEN IS_NULLABLE = 'NO' THEN 'NOTNULL' ELSE 'DEFAULTNULL' END,
+            CASE WHEN COLUMN_DEFAULT IS NOT NULL THEN CONCAT('DEFAULT', COLUMN_DEFAULT) ELSE '' END,
+            ','
+            ORDER BY ORDINAL_POSITION
+        ),
+        'PRIMARYKEY(`id`)',
+        ')ENGINE=InnoDBDEFAULTCHARSET=utf8mb4COLLATE=utf8mb4_0900_ai_ci'
     ) AS 'msg'
 FROM
     INFORMATION_SCHEMA.COLUMNS
