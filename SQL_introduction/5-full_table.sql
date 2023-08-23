2b9047159abcbe9096f9f751f18b725b77bd0f73
GustaveIR <iraguhagustave@gmail.com>
2023-08-23 16:09:25 +0200

    SQL_introduction

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,8 +1,3 @@
--- Full description
--- Write a script that prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server.
--- The database name will be passed as an argument of the mysql command
--- You are not allowed to use the DESCRIBE or EXPLAIN statements
-
 -- Script to print the full description of the table first_table
 SELECT CONCAT(
     'CREATE TABLE `', TABLE_NAME, '` (',
@@ -15,8 +10,8 @@ SELECT CONCAT(
         IF(EXTRA = 'auto_increment', ' AUTO_INCREMENT', '')
         SEPARATOR ',\n'
     ),
-    ') ', ENGINE, ' DEFAULT CHARSET=', CHARACTER_SET_NAME, ' COLLATE=', COLLATION_NAME
+    ') DEFAULT CHARSET=', CHARACTER_SET_NAME, ' COLLATE=', COLLATION_NAME
 ) AS Table_Description
 FROM INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table'
-GROUP BY TABLE_NAME, ENGINE, CHARACTER_SET_NAME, COLLATION_NAME;
+GROUP BY TABLE_NAME, CHARACTER_SET_NAME, COLLATION_NAME;
