6d66600e8aaf0431b7681585f43e28caba70242c
Gustave IRAGUHA <40911280+GustaveIR@users.noreply.github.com>
2023-08-28 14:30:11 +0200

    SQL_introduction

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,9 +1,27 @@
--- Script to print the full description of the table first_table
 SELECT CONCAT(
     'Table   Create Table\n',
     TABLE_NAME, ' ',
-    CREATE_TABLE
+    CREATE_TABLE_STMT
 ) AS Table_Description
-FROM INFORMATION_SCHEMA.TABLES
-WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
-
+FROM (
+    SELECT
+        TABLE_NAME,
+        CONCAT(
+            'CREATE TABLE `', TABLE_NAME, '` (\n',
+            GROUP_CONCAT(
+                '  `', COLUMN_NAME, '` ',
+                COLUMN_TYPE,
+                IF(IS_NULLABLE = 'NO', ' NOT NULL', ''),
+                IF(COLUMN_DEFAULT IS NOT NULL, CONCAT(' DEFAULT ', COLUMN_DEFAULT), ''),
+                IF(EXTRA = 'auto_increment', ' AUTO_INCREMENT', ''),
+                '\n'
+                ORDER BY ORDINAL_POSITION
+                SEPARATOR ''
+            ),
+            'PRIMARY KEY (`', GROUP_CONCAT(COLUMN_NAME ORDER BY ORDINAL_POSITION SEPARATOR '`, `'), '`)\n',
+            ') ENGINE=', ENGINE, ' DEFAULT CHARSET=', TABLE_COLLATION
+        ) AS CREATE_TABLE_STMT
+    FROM INFORMATION_SCHEMA.COLUMNS
+    WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table'
+    GROUP BY TABLE_NAME
+) AS Table_Info;
