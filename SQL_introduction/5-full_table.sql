368cfbe3513ab880689fde781749aaef4b76db84
Gustave IRAGUHA <40911280+GustaveIR@users.noreply.github.com>
2023-08-28 14:33:17 +0200

    SQL_introductin

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,8 +1,6 @@
 -- Script to print the full description of the table first_table
 SELECT CONCAT(
-    'Table   Create Table\n',
-    TABLE_NAME, ' ',
-    CREATE_TABLE_STMT
+    TABLE_NAME, CREATE_TABLE_STMT, '\n'
 ) AS Table_Description
 FROM (
     SELECT
@@ -10,19 +8,19 @@ FROM (
         CONCAT(
             'CREATE TABLE `', TABLE_NAME, '` (\n',
             GROUP_CONCAT(
-                '  `', COLUMN_NAME, '` ',
-                COLUMN_TYPE,
+                '  `', COLUMN_NAME, '` ', COLUMN_TYPE,
                 IF(IS_NULLABLE = 'NO', ' NOT NULL', ''),
                 IF(COLUMN_DEFAULT IS NOT NULL, CONCAT(' DEFAULT ', COLUMN_DEFAULT), ''),
                 IF(EXTRA = 'auto_increment', ' AUTO_INCREMENT', ''),
-                '\n'
+                ',\n'
                 ORDER BY ORDINAL_POSITION
                 SEPARATOR ''
             ),
             'PRIMARY KEY (`', GROUP_CONCAT(COLUMN_NAME ORDER BY ORDINAL_POSITION SEPARATOR '`, `'), '`)\n',
-            ') ENGINE=', ENGINE, ' DEFAULT CHARSET=', TABLE_COLLATION
+            ') ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci'
         ) AS CREATE_TABLE_STMT
     FROM INFORMATION_SCHEMA.COLUMNS
     WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table'
     GROUP BY TABLE_NAME
 ) AS Table_Info;
+
