0f29f3faac1dc089b108b25d170fa0e8f887392b
Gustave IRAGUHA <40911280+GustaveIR@users.noreply.github.com>
2023-08-28 10:33:08 +0200

    SQL_introduction

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,12 +1,9 @@
 -- Script to print the full description of the table first_table
 SELECT CONCAT(
-    able: t
-Create Table: CREATE TABLE `t` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `s` char(60) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+    'Table   Create Table\n',
+    TABLE_NAME, ' ',
+    CREATE_TABLE
 ) AS Table_Description
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table'
-GROUP BY TABLE_NAME, CHARACTER_SET_NAME, COLLATION_NAME;
+FROM INFORMATION_SCHEMA.TABLES
+WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
+
