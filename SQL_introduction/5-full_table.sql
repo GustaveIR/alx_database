2cbd35b8444147ba31552f2536cb75c5f9dc7d95
Gustave IRAGUHA <40911280+GustaveIR@users.noreply.github.com>
2023-08-30 18:45:57 +0200

    SQL_introduction

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,7 +1,7 @@
 -- Script to print the full description of the table first_table
 USE hbtn_0c_0;
 
-SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_DEFAULT
+SELECT GROUP_CONCAT(COLUMN_NAME, ' ', COLUMN_TYPE, ' ', IFNULL(COLUMN_DEFAULT, 'NULL')) AS column_info
 FROM INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_SCHEMA = 'hbtn_0c_0'
   AND TABLE_NAME = 'first_table';
