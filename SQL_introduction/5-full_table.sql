2d73fb785ec2ab71ac95b6d9c07b28b67cb982fc
Gustave IRAGUHA <40911280+GustaveIR@users.noreply.github.com>
2023-09-04 12:59:54 +0200

    SQL_introduction

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,11 +1,12 @@
 -- Script to print the full description of the table first_table
-first_table CREATE TABLE `first_table` (
-`id` int NOT NULL AUTO_INCREMENT,
-`name` varchar(128) DEFAULT NULL,
-`c` char(1) DEFAULT NULL,
-`created_at` date DEFAULT NULL,
-PRIMARY KEY (`id`)
+CREATE TABLE `first_table` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(128) DEFAULT NULL,
+  `c` char(1) DEFAULT NULL,
+  `created_at` date DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+
 
 
 
