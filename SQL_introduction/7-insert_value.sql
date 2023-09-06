956bbaa06334c05895786c76adac4ffea9e4a304
Gustave IRAGUHA <40911280+GustaveIR@users.noreply.github.com>
2023-09-06 18:37:37 +0200

    SQL_introduction

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,3 +1,3 @@
--- inserts a new_record in a row in 'first_table' from database 'hbtn_0c_0'
+-- inserts a new record in a row in 'first_table' from database 'hbtn_0c_0'
 
-INSERT INTO 'first_table' ('id' , 'name') VALUES (89, 'Holberton School');
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, 'Holberton School');
