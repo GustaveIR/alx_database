36d5e77d52315444cbec2281890e64eea0607c3d
Gustave IRAGUHA <40911280+GustaveIR@users.noreply.github.com>
2023-09-06 20:52:25 +0200

    SQL_more_queries

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,5 +1,3 @@
--- List all privileges of MySQL user user_0d_1 na user_0d_2
-
-
-SHOW GRANT FOR 'user_0d_1'@'localhost';
-SHOW GRANT FOR 'user_0d_2'@'localhost';
+-- List privileges for user_0d_1 and user_0d_2
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
