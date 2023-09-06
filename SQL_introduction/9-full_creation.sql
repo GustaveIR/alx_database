3e1bbac2ea542bbb7c93bc1473c92a9650980b92
Gustave IRAGUHA <40911280+GustaveIR@users.noreply.github.com>
2023-09-06 19:42:58 +0200

    SQL_introduction

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,9 +1,13 @@
---Create_table 'second_table' in database 'hbtn_0c_0'
---Add-props (id INT), (name VAR-CHAR(256)), (score INT)
-CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+-- Create table second_table if it doesn't exist
+CREATE TABLE IF NOT EXISTS `second_table` (
+    `id` INT,
+    `name` VARCHAR(256),
+    `score` INT
+);
 
---Script should create maximum of 4 records 
-INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "John", 10);
-INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, "Alex", 3);
-INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (3, "Bob", 14);
-INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (4, "George", 8);
+-- Insert the specified records
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES
+    (1, "John", 10),
+    (2, "Alex", 3),
+    (3, "Bob", 14),
+    (4, "George", 8);
