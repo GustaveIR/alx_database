b90c5dca77d6094e5ec865d2bc1cb0b57ca2f486
Gustave IRAGUHA <40911280+GustaveIR@users.noreply.github.com>
2023-09-07 05:11:53 +0200

    SQL_more_queries

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,11 @@
--- Create the database of hbtn_0d_2 a& user user_0d_2
-CREATE DATABASE IF NOT EXISTS hbtn_0d_2
+-- Create the database hbtn_0d_2 if it doesn't exist
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+-- Create the MySQL server user user_0d_2
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON  hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+
+-- Grant only SELECT privilege in the database hbtn_0d_2 to user_0d_2
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+
+-- Flush privileges to apply the changes
 FLUSH PRIVILEGES;
