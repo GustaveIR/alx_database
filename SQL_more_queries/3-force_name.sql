30d7cb204ca2ed8f98ea02181c189a9ca3b68c14
Gustave IRAGUHA <40911280+GustaveIR@users.noreply.github.com>
2023-09-07 17:50:26 +0200

    SQL_more_queries

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -11,11 +11,3 @@ INSERT INTO force_name (name) VALUES ('Python is cool');
 INSERT INTO force_name (name) VALUES ('Holberton');
 INSERT INTO force_name (name) VALUES ('School');
 INSERT INTO force_name (name) VALUES ('C is fun');
--- Regular inserts + select
-INSERT INTO force_name (id, name) VALUES (1, 'Holberton School');
-INSERT INTO force_name (id, name) VALUES (2, 'Holberton');
-INSERT INTO force_name (id, name) VALUES (3, 'School');
-INSERT INTO force_name (id, name) VALUES (4, 'C is fun');
-INSERT INTO force_name (id, name) VALUES (1, 'Python is cool');
-
-SELECT * FROM force_name ORDER BY id, name ASC;
