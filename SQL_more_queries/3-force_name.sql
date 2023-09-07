bbdab6d38cdea93fe5aa5d0125a249fb31562519
Gustave IRAGUHA <40911280+GustaveIR@users.noreply.github.com>
2023-09-07 17:48:33 +0200

    SQL_more_queries

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,7 +1,7 @@
 -- CREATE TABLE and INSERTs should be in uppercase
 -- create table force_name on MySQL server
 CREATE TABLE IF NOT EXISTS force_name (
-    id INT AUTO_INCREMENT,  -- Set id as the primary key and auto-incremented
+    id INT PRIMARY KEY AUTO_INCREMENT,  -- Set id as the primary key and auto-incremented
     name VARCHAR(256) NOT NULL
 );
 
@@ -11,3 +11,11 @@ INSERT INTO force_name (name) VALUES ('Python is cool');
 INSERT INTO force_name (name) VALUES ('Holberton');
 INSERT INTO force_name (name) VALUES ('School');
 INSERT INTO force_name (name) VALUES ('C is fun');
+-- Regular inserts + select
+INSERT INTO force_name (id, name) VALUES (1, 'Holberton School');
+INSERT INTO force_name (id, name) VALUES (2, 'Holberton');
+INSERT INTO force_name (id, name) VALUES (3, 'School');
+INSERT INTO force_name (id, name) VALUES (4, 'C is fun');
+INSERT INTO force_name (id, name) VALUES (1, 'Python is cool');
+
+SELECT * FROM force_name ORDER BY id, name ASC;
