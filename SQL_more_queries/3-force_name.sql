a65c00cf1e2fe0f0452afaf327d8d0c4510385c6
Gustave IRAGUHA <40911280+GustaveIR@users.noreply.github.com>
2023-09-08 21:19:36 +0200

    SQL_more_queries

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,12 +1,6 @@
 -- create table force_name on MySQL server
-CREATE TABLE IF NOT EXISTS force_name (
-    id INT PRIMARY KEY AUTO_INCREMENT,  -- Set id as the primary key and auto-incremented
-    name VARCHAR(256) NOT NULL
-);
+CREATE TABLE IF NOT EXISTS force_name ( id INT PRIMARY KEY, name VARCHAR(256) NOT NULL);
+     
+    
 
--- Inserting data into force_name
-INSERT INTO force_name (name) VALUES ('Holberton School');
-INSERT INTO force_name (name) VALUES ('Python is cool');
-INSERT INTO force_name (name) VALUES ('Holberton');
-INSERT INTO force_name (name) VALUES ('School');
-INSERT INTO force_name (name) VALUES ('C is fun');
+
