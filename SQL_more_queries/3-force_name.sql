ee70d4e3c6755d0a89d499ea1095140e61fb214c
Gustave IRAGUHA <40911280+GustaveIR@users.noreply.github.com>
2023-09-12 10:58:48 +0200

    SQL_more_queries

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,5 +1,5 @@
 -- Create table force_name on MySQL server if it doesn't exist
-CREATE TABLE IF NOT EXISTS id_not_null (
+CREATE TABLE IF NOT EXISTS force_name (
     id INT PRIMARY KEY,
-    name VARCHAR(256) 
+    name VARCHAR(256) NOT NULL
 );
