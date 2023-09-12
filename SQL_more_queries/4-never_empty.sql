dd3148d6a561a535e6f6c93f3efb2eed6e9610df
Gustave IRAGUHA <40911280+GustaveIR@users.noreply.github.com>
2023-09-12 12:51:27 +0200

    SQL_more_queries

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -4,9 +4,3 @@ CREATE TABLE IF NOT EXISTS id_not_null (
     name VARCHAR(256)
 );
 
--- Insert data into id_not_null table
-INSERT INTO id_not_null (name) VALUES ('Holberton');
-INSERT INTO id_not_null (id, name) VALUES (2, 'Holberton School');
-INSERT INTO id_not_null (id, name) VALUES (3, 'Python is cool');
-INSERT INTO id_not_null (id, name) VALUES (4, 'School');
-INSERT INTO id_not_null (id, name) VALUES (5, 'C is fun');
