c2a6d725fd56dbce7fe96b5f2af77a35992850af
Gustave IRAGUHA <40911280+GustaveIR@users.noreply.github.com>
2023-09-12 11:07:36 +0200

    SQL_more_queries

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,15 +1,7 @@
--- Drop the table if it exists
-DROP TABLE IF EXISTS force_name;
-
--- Create table force_name on MySQL server
-CREATE TABLE force_name (
+-- Create table force_name on MySQL server if it doesn't exist
+CREATE TABLE IF NOT EXISTS force_name (
     id INT PRIMARY KEY,
     name VARCHAR(256) NOT NULL
 );
 
--- Insert data into force_name table
-INSERT INTO force_name (id, name) VALUES (1, 'Holberton School');
-INSERT INTO force_name (id, name) VALUES (2, 'Python is cool');
-INSERT INTO force_name (id, name) VALUES (3, 'Holberton');
-INSERT INTO force_name (id, name) VALUES (4, 'School');
-INSERT INTO force_name (id, name) VALUES (5, 'C is fun');
+
