340201072218a7db769433b924f1d46c0e7b528b
Gustave IRAGUHA <40911280+GustaveIR@users.noreply.github.com>
2023-09-12 11:32:17 +0200

    SQL_more_queries

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,12 +1,5 @@
--- Create table FORCE_NAME on MySQL server if it doesn't exist
-CREATE TABLE IF NOT EXISTS FORCE_NAME (
-    ID INT PRIMARY KEY,
-    NAME VARCHAR(256) NOT NULL
+-- Create table force_name on MySQL server if it doesn't exist
+CREATE TABLE IF NOT EXISTS force_name (
+    id INT PRIMARY KEY,
+    name VARCHAR(256) NOT NULL
 );
-
--- Insert data into FORCE_NAME table
-INSERT INTO FORCE_NAME ID, NAME VALUES (1, 'Holberton School');
-INSERT INTO FORCE_NAME ID, NAME VALUES (1, 'Python is cool');
-INSERT INTO FORCE_NAME ID, NAME VALUES (2, 'Holberton');
-INSERT INTO FORCE_NAME ID, NAME VALUES (3, 'School');
-INSERT INTO FORCE_NAME ID, NAME VALUES (4, 'C is fun');
