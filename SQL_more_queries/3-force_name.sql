84b679feb1a85bc6b7b92ee02923b51e0830a607
Gustave IRAGUHA <40911280+GustaveIR@users.noreply.github.com>
2023-09-12 11:01:44 +0200

    SQL_more_queries

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -3,3 +3,10 @@ CREATE TABLE IF NOT EXISTS force_name (
     id INT PRIMARY KEY,
     name VARCHAR(256) NOT NULL
 );
+
+-- Insert data into force_name table
+INSERT INTO force_name (id, name) VALUES (1, 'Holberton School');
+INSERT INTO force_name (id, name) VALUES (2, 'Python is cool');
+INSERT INTO force_name (id, name) VALUES (3, 'Holberton');
+INSERT INTO force_name (id, name) VALUES (4, 'School');
+INSERT INTO force_name (id, name) VALUES (5, 'C is fun');
