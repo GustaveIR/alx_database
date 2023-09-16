9f874bdf35a3efe767dd7fd0e6e17ca620a05413
Gustave IRAGUHA <40911280+GustaveIR@users.noreply.github.com>
2023-09-16 11:36:27 +0200

    SQL_more_queries

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,7 +1,6 @@
--- Creates table UNIQUE_ID on MySQL server.
-
-
+-- Create table UNIQUE_ID if it doesn't exist
 CREATE TABLE IF NOT EXISTS UNIQUE_ID (
-    id INT DEFAULT 1, UNIQUE(ID),
-    name VARCHAR(256) );
-
+    id INT DEFAULT 1,
+    name VARCHAR(256),
+    UNIQUE (id)
+);
