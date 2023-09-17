d8bd861dbd5e1ba05fe2e327f36065c78ab97283
Gustave IRAGUHA <40911280+GustaveIR@users.noreply.github.com>
2023-09-17 14:49:02 +0200

    SQL_more_queries

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,5 +1,6 @@
 --creates databases hbtn_0d_usa & table states on my MySQL server.
-CREATE TABLE IF NOT EXISTS states (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
+    id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(256) NOT NULL
 );
