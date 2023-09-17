04534ca8aef29e4c507579960846df2a89f09f9a
Gustave IRAGUHA <40911280+GustaveIR@users.noreply.github.com>
2023-09-17 14:25:32 +0200

    SQL_more_queries

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,5 +1,5 @@
 -- Create table UNIQUE_ID if it doesn't exist
-CREATE TABLE IF NOT EXISTS UNIQUE_ID (
+CREATE TABLE IF NOT EXISTS unique_id (
     id INT DEFAULT 1,
     name VARCHAR(256),
     UNIQUE (id)
