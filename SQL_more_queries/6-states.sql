566f0ffb700f57a2121b67b6b44980a0a3d2750a
Gustave IRAGUHA <40911280+GustaveIR@users.noreply.github.com>
2023-09-28 17:44:24 +0200

    SQL_more_queries

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,8 +1,15 @@
 --creates databases hbtn_0d_usa & table states on my MySQL server.
 
 
+-- Create the database if it doesn't exist
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
-       id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
-       name VARCHAR(256) NOT NULL
+USE hbtn_0d_usa;
+
+-- Create the states table if it doesn't exist
+CREATE TABLE IF NOT EXISTS states (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(256) NOT NULL
 );
+
+-- Insert data into the states table (optional)
+INSERT INTO states (name) VALUES ("California"), ("Arizona"), ("Texas");
