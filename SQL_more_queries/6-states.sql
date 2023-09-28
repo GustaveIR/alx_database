0497aa3034a034275a9741ac4d1071ddf8f9fcd7
Gustave IRAGUHA <40911280+GustaveIR@users.noreply.github.com>
2023-09-28 18:00:09 +0200

    SQL_more_queries

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -6,13 +6,3 @@ CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
        id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
        name VARCHAR(256) NOT NULL
 );
-
--- Regular inserts + select
-INSERT INTO states (id, name) VALUES (1, "California");
-INSERT INTO states (id, name) VALUES (2, "Arizona");
-INSERT INTO states (name) VALUES ("Nevada");
-INSERT INTO states (name) VALUES ("Illinois");
-INSERT INTO states (id, name) VALUES (10, "New York");
-INSERT INTO states (name) VALUES ("Louisiana");
-
-SELECT * FROM states ORDER BY id ASC;
