44af43c6d77172fb9179c4e56c53cb39200c324e
Gustave IRAGUHA <40911280+GustaveIR@users.noreply.github.com>
2023-09-28 22:36:09 +0200

    SQL_more_queries

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,6 +1,9 @@
 -- creates databases hbtn_0d_usa & table cities on my MySQL server.
 
+-- Create the database if it doesn't exist
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+-- Use the database
 USE hbtn_0d_usa;
 
 -- Create the cities table if it doesn't exist
@@ -11,8 +14,20 @@ CREATE TABLE IF NOT EXISTS cities (
     FOREIGN KEY (state_id) REFERENCES states(id)
 );
 
--- Insert data into the cities table
+-- Attempt to insert valid data
 INSERT INTO cities (state_id, name) VALUES (1, "San Francisco");
 
--- Select data from the cities table
+-- Attempt to insert invalid data (foreign key violation)
+INSERT INTO cities (state_id, name) VALUES (10, "Paris");
+
+-- Attempt to insert invalid data (missing state_id)
+INSERT INTO cities (name) VALUES ("New York");
+
+-- Attempt to insert invalid data (missing name)
+INSERT INTO cities (state_id) VALUES (2);
+
+-- Attempt to insert invalid data (both state_id and name missing)
+INSERT INTO cities () VALUES ();
+
+-- Display the content of the cities table
 SELECT * FROM cities;
